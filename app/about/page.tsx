23e6fa34f41d9a03d464e80a7113c21654963663
Trevor Hackett <1577822+trevor-hackett@users.noreply.github.com>
2023-01-24 22:45:30 -0700

    Dummy Pages

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div className="flex items-center justify-center text-4xl sm:text-5xl uppercase text-center">
+      Work in progress
+    </div>
+  );
+}
