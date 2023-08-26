41997334cb5f057653a4e9e2b5a02089f910a9bd
Trevor Hackett <1577822+trevor-hackett@users.noreply.github.com>
2023-08-26 15:32:13 -0600

    Update dependencies

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-5ZJJX2NQ.mjs
@@ -0,0 +1,87 @@
+// contentlayer.config.js
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import readingTime from "reading-time";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import remarkGfm from "remark-gfm";
+import rehypeCodeTitles from "rehype-code-titles";
+import rehypePrism from "rehype-prism-plus";
+var computedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => `/${doc._raw.flattenedPath}`
+  },
+  slugAsParams: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/")
+  },
+  readingTime: { type: "json", resolve: (doc) => readingTime(doc.body.raw) }
+};
+var Page = defineDocumentType(() => ({
+  name: "Page",
+  filePathPattern: `pages/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string"
+    }
+  },
+  computedFields
+}));
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `posts/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string"
+    },
+    date: {
+      type: "date",
+      required: true
+    },
+    category: {
+      type: "string",
+      required: true
+    },
+    image: {
+      type: "string",
+      required: false
+    }
+  },
+  computedFields
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Post, Page],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeCodeTitles,
+      rehypePrism,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["anchor"]
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  Page,
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-5ZJJX2NQ.mjs.map
